78b8c5f3cb1da60070994ed2c21caa0c0736966f
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-08 11:00:43 -0300

    feat refactor and style: I can't be bothered to separate these right now. I implemented a asteroid field, and reorganized the code a bit for style

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
-
-    clock = pygame.time.Clock()
-    dt = 0
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
 
     while True:
         log_state()
@@ -29,13 +31,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         updatable.update(dt)
-        for object in drawable:
-            object.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
