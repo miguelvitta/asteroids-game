b3542fb677465ae846e125604aea122a3ed223e8
Miguel Vitta <miguelvittadev@gmail.com>
2026-01-09 16:59:04 -0300

    feat: implements a cooldown so the shots are not a barrage

--- player.py
+++ player.py
@@ -6,13 +6,16 @@ from constants import (
     PLAYER_SHOOT_SPEED,
     PLAYER_SPEED,
     PLAYER_TURN_SPEED,
+    PLAYER_SHOOT_COOLDOWN_SECONDS,
 )
 from shot import Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.player_shot_cooldon_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
@@ -27,6 +30,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.player_shot_cooldon_timer -= dt
 
         if keys[pygame.K_w]:
             self.move(dt)
@@ -49,6 +53,8 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.player_shot_cooldon_timer > 0:
+            return False
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
-
+        self.player_shot_cooldon_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
